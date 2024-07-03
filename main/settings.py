a17b4b1155cd35c128b878a546b0e08fc509e8a2
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2024-07-03 21:12:00 +0300

    Connect to postgresql db

--- main/settings.py
+++ main/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "person_app",
+        "USER":config("USER"),
+        "PASSWORD": config("PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 
 
